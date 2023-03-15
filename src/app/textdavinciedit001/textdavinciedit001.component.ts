e4d2cdf08796a20f7fda6355a777e710f8e5eadb
Juan Daniel Rodríguez Oropeza <j_danny07@hotmail.com>
2023-03-15 14:24:49 -0600

    Variable de entorno (#13)
    * imagesgeneration listo
    
    * Update .gitignore
    
    * Textedit listo
    
    * moderations listo
    
    * Variables de entorno
    
    * Update tsconfig.spec.json
    
    * Pruebas actualizadas

--- src/app/textdavinciedit001/textdavinciedit001.component.ts
+++ src/app/textdavinciedit001/textdavinciedit001.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Textdavinciedit001Service } from '../services/textdavinciedit001/textdavinciedit001.service';
 
 @Component({
   selector: 'app-textdavinciedit001',
   templateUrl: './textdavinciedit001.component.html',
   styleUrls: ['./textdavinciedit001.component.css']
 })
-export class Textdavinciedit001Component {
+export class Textdavinciedit001Component implements OnInit {
+  constructor(private textdavinciedit001 : Textdavinciedit001Service) {}
 
+  ngOnInit(): void {
+  }
+
+  result: string = "";
+  input: string = "";
+
+  postCompletition() {
+      var payload = {
+        model: "text-davinci-edit-001",
+        input: this.input,
+        instruction: "Fix the spelling mistakes"
+      }
+
+      this.textdavinciedit001.postCompletion(payload)
+      .subscribe((data: any) => {
+        //alert(JSON.stringify(data));
+        console.log(data);
+        this.result = data.choices[0].text;
+      });
+  }
 }
